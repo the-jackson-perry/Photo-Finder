42fe661731282ee78218aaabfab1905e99e911e1
the-jackson-perry <jdp99@pitt.edu>
2019-03-02 10:46:54 -0500

    improved/added options and file types to find

--- Photo-Finder.py
+++ Photo-Finder.py
@@ -11,12 +11,11 @@ def updateCount():
 	global filesFound
 
 	if (time()-lastUpdated>10):
-		print(filesFound, "files found...")
+		print(filesFound, "files found so far...")
 		lastUpdated = time()
 
 def FindType(FileExtension, root, destination):
 	global filesFound
-	updateCount()
 	chdir(root)
 	dirs = []
 
@@ -24,34 +23,45 @@ def FindType(FileExtension, root, destination):
 		all = listdir()
 		for name in all:
 			if(name.endswith(FileExtension)):
-				copyfile(getcwd()+"/"+name, destDirectory+"/"+name)
+				copyfile(getcwd()+"/"+name, destination+"/"+name)
 				filesFound += 1
-			if(path.isdir(getcwd()+"/"+name)):
+			if(path.isdir(getcwd()+"/"+name) and (getcwd()+"/"+name != destination)):
 				dirs.append(name)
 
 		for subdir in dirs:
 			FindType(FileExtension, subdir, destination)
 	except:
-		updateCount()
-
+		filesFound+=0
+	updateCount()
 	chdir("..")
 	# end of FindType()
 
 
 
 mode = input("Default or manual mode?")
-if(mode.lower()=="default"):
-	destDirectory = "C:/Users/Jackson/Desktop/Photos_Found"
-	rootDir = "C:/Users"
-else:
+if(mode.lower()=="manual"):
 	rootDir = input("Enter path to root folder\n")
 	destDirectory = input("Enter path to destination folder:\n")
+	if(rootDir.lower()=="test"):
+		rootDir = "C:/Users/Jackson/Documents/hackathonTest"
+	if(destDirectory.lower()=="test"):
+		destDirectory = "C:/Users/Jackson/Desktop/Photos-Found"
+else:
+	lastDir = getcwd()
+	while(not getcwd().endswith("Users")):
+		lastDir = getcwd()
+		chdir("..")
+	rootDir = getcwd()
+	destDirectory = lastDir + "/Desktop/Photos-Found"
 
 
 if(not path.isdir(destDirectory)):
 	makedirs(destDirectory)
 
-extensions = [".jpg", ".jpeg", ".gif", ".bmp", ".png"]
+extensions = [".jpg", ".jpeg", ".gif", ".bmp", ".png", ".avi", ".flv", ".wmv", ".mov", ".mp4"]
 
 for ext in extensions:
 	FindType(ext, rootDir, destDirectory)
+
+
+print("Process complete: ", filesFound, "files were found.")
