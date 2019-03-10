12d929ec4e8b3b85b3d4f53ce6ed8907af43b9e9
the-jackson-perry <jdp99@pitt.edu>
2019-03-10 19:26:49 -0400

    Added feature to put files into subfolders of a maximum size

--- Photo-Finder.py
+++ Photo-Finder.py
@@ -1,9 +1,11 @@
 from os import *
-from shutil import copyfile
+from shutil import copyfile, move
 from time import time
 
-lastUpdated = time()
+extensions = [".jpg", ".jpeg", ".gif", ".bmp", ".png", ".avi", ".flv", ".wmv", ".mov", ".mp4"]
 
+
+lastUpdated = time()
 filesFound = 0
 
 def updateCount():
@@ -36,16 +38,62 @@ def FindType(FileExtension, root, destination):
 	chdir("..")
 	# end of FindType()
 
+def getFilesOnly(directory, extensions):
+	oldDir = getcwd()
+	chdir(directory)
+
+	allItems = listdir()
+	files = []
+	totalSize = 0
+	for item in allItems:
+		for ext in extensions:
+			if(item.endswith(ext)):
+				files.append(item)
+
+	chdir(oldDir)
+	return files
+
+def totalFileSize(directory, extensions):
+	oldDir = getcwd()
+	chdir(directory)
+
+	allItems = listdir()
+	totalSize = 0
+	for item in allItems:
+		for ext in extensions:
+			if(item.endswith(ext)):
+				totalSize += stat(getcwd()+"/"+item).st_size
+
+	chdir(oldDir)
+	return totalSize
+
+def organize(destDirectory, extensions):
+	subDirSize = int(input("Maximum size(in bytes) of each folder: "))
+
+	chdir(destDirectory)
+	files = getFilesOnly(getcwd(), extensions)
+	totalSize = totalFileSize(getcwd(), extensions)
+	subDirCount = 1
+	makedirs(destDirectory+"/"+str(subDirCount))
+	while(totalSize>0):
+		if(totalFileSize(destDirectory+"/"+str(subDirCount), extensions) + stat(destDirectory+"/"+files[0]).st_size < subDirSize):
+			totalSize -= stat(destDirectory+"/"+files[0]).st_size
+			move(getcwd()+"/"+files[0], getcwd()+"/"+str(subDirCount)+"/"+files[0])
+			files.remove(files[0])
+
+		else:
+			subDirCount += 1
+			makedirs(destDirectory+"/"+str(subDirCount))
 
 
-mode = input("Default or manual mode?")
-if(mode.lower()=="manual"):
+
+mode = input("Default or manual mode?").lower()
+if(mode=="test"):
+	rootDir = "C:/Users/Jackson/Documents/hackathonTest"
+	destDirectory = "C:/Users/Jackson/Desktop/Photos-Found"
+elif(mode=="manual"):
 	rootDir = input("Enter path to root folder\n")
-	destDirectory = input("Enter path to destination folder:\n")
-	if(rootDir.lower()=="test"):
-		rootDir = "C:/Users/Jackson/Documents/hackathonTest"
-	if(destDirectory.lower()=="test"):
-		destDirectory = "C:/Users/Jackson/Desktop/Photos-Found"
+	destDirectory = input("Enter path to destination folder:\n")		
 else:
 	lastDir = getcwd()
 	while(not getcwd().endswith("Users")):
@@ -54,14 +102,13 @@ else:
 	rootDir = getcwd()
 	destDirectory = lastDir + "/Desktop/Photos-Found"
 
-
 if(not path.isdir(destDirectory)):
 	makedirs(destDirectory)
-
-extensions = [".jpg", ".jpeg", ".gif", ".bmp", ".png", ".avi", ".flv", ".wmv", ".mov", ".mp4"]
 
 for ext in extensions:
 	FindType(ext, rootDir, destDirectory)
 
 
 print("Process complete: ", filesFound, "files were found.")
+
+organize(destDirectory, extensions)
